21d46d3306c88ab691bfa82014be6541d5a4a48e
ryanlobes13 <90591769+ryanlobes13@users.noreply.github.com>
2021-09-19 15:01:00 -0500

    The first attempt of switch

--- forms/Ifelse/Ifelse.js
+++ forms/Ifelse/Ifelse.js
@@ -1,11 +1,15 @@
+/*
+//creating variables for the inputs
 let name = prompt('Enter your first name here:')
 
 let state = prompt('Enter your state you live (two capitalized letters):')
 
 let temp = prompt('Enter the current outside temperature (Fahrenheit):')
 
+//creating array to hold for each message
 let messages = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you won\'t need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
 
+//if else statements to create a response of what to wear based on the inputs for state and temperature 
 if (temp <= 32 && state == 'NE') {
   console.log(`${name} you should ${messages[0]}`)}
 
@@ -19,4 +23,5 @@ else if (temp < 70 && temp > 50 && state == 'FL') {
   console.log(`${name} you should ${messages[3]}`)}
   
 else  
-  console.log(`Undefined Weather`);
+  console.log(`Undefined Weather or State.`);
+  */
--- forms/Switch/Switch.js
+++ forms/Switch/Switch.js
@@ -1,7 +1,35 @@
-/*let name = prompt('Enter your first name here:')
+
+let name = prompt('Enter your first name here:')
 
 let state = prompt('Enter your state you live (two capitalized letters):')
 
 let temp = prompt('Enter the current outside temperature (Fahrenheit):')
 
-*/
+let messages = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you won\'t need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
+
+
+
+switch (true) {
+  case temp <= 32 && state == 'NE':
+    console.log(`${name} you should ${messages[0]}`);
+    break;
+  
+  case temp > 32 && temp <= 50 && state == 'NE':
+    console.log(`${name} you should ${messages[1]}`)
+    break;
+    
+  case temp > 32 && temp <= 50 && state == 'FL':
+    console.log(`${name} you should ${messages[2]}`)
+    break;
+    
+  case temp > 50 && temp <= 70 && state == 'FL':
+    console.log(`${name} you should ${messages[3]}`)
+    break;
+  
+  default:
+    console.log(`Undefined Weather or State.`)
+    break;
+  }
+  
+  
+  
