f900fc83592ea15518a3a5bf46e605fb3e226d52
ryanlobes13 <90591769+ryanlobes13@users.noreply.github.com>
2021-09-19 13:57:03 -0500

    If else works

--- forms/Ifelse/Ifelse.js
+++ forms/Ifelse/Ifelse.js
@@ -4,19 +4,19 @@ let state = prompt('Enter your state you live (two capitalized letters):')
 
 let temp = prompt('Enter the current outside temperature (Fahrenheit):')
 
-let messages = ['Wear a warm coat, hat, scarf and gloves.', 'Wear a warm coat but you won't need a hat, scarf or gloves.', 'Wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'Wear a warm coat, hat and gloves. Maybe a scarf too.']
+let messages = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you won\'t need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
 
-if (temp <= 32) {
-  console.log(`${name} you should ${messages[0]}`)}:
+if (temp <= 32 && state == 'NE') {
+  console.log(`${name} you should ${messages[0]}`)}
 
-else if (temp < 50 && temp > 32 && state == NE){
-  console.log(`${name} you should ${messages[1]}`)}:
+else if (temp < 50 && temp > 32 && state == 'NE') {
+  console.log(`${name} you should ${messages[1]}`)}
   
-else if (temp < 50 && temp > 32 && state == FL){
-  console.log(`${name} you should ${messages[2]}`)}:
+else if (temp < 50 && temp > 32 && state == 'FL') {
+  console.log(`${name} you should ${messages[2]}`)}
 
-else if (temp < 70 && temp > 50 && state == FL){
-  console.log(`${name} you should ${messages[3]}`)}:
+else if (temp < 70 && temp > 50 && state == 'FL') {
+  console.log(`${name} you should ${messages[3]}`)}
   
-else { 
-  console.log(`Undefined Weather`)};
+else  
+  console.log(`Undefined Weather`);
